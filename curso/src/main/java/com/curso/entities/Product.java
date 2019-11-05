9196279ebcd035cbde6016b09cba90d677082c54
João Paulo Peixoto <cdpcunha@hotmail.com>
2019-11-05 14:10:30 -0300

    Criando associacoes Many to Many com JoinTable

--- curso/src/main/java/com/curso/entities/Product.java
+++ curso/src/main/java/com/curso/entities/Product.java
@@ -8,15 +8,17 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 
 @Entity
-@Table(name="TB_PRODUCT")
+@Table(name = "TB_PRODUCT")
 public class Product implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
@@ -24,10 +26,13 @@ public class Product implements Serializable {
 	private String description;
 	private Double price;
 	private String imgURL;
-	
-	@Transient
+
+	@ManyToMany
+	@JoinTable(name = "TB_PROD_CATE",
+	joinColumns = @JoinColumn(name = "product_id"),
+	inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
-	
+
 	public Product(Integer id, String name, String description, Double price, String imgURL) {
 		this.id = id;
 		this.name = name;
@@ -35,9 +40,9 @@ public class Product implements Serializable {
 		this.price = price;
 		this.imgURL = imgURL;
 	}
-	
-	public Product() {	
-}
+
+	public Product() {
+	}
 
 	public Integer getId() {
 		return id;
@@ -103,5 +108,5 @@ public class Product implements Serializable {
 		if (id != other.id)
 			return false;
 		return true;
-	}	
+	}
 }
